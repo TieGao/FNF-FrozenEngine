eabbc5be4e00e84194124d0ca66035d6f8a32c0d
TieGao <eeg2332019@outlook.com>
2025-12-13 23:03:08 +0800

    minor fix

--- setup/windows.bat
+++ setup/windows.bat
@@ -4,13 +4,13 @@ cd ..
 @echo on
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib install lime 8.2.3
-haxelib install openfl 9.4.2
-haxelib install flixel 5.8.0
-haxelib install flixel-addons 3.3.2
-haxelib install flixel-tools 1.5.1
-haxelib install hscript-iris 1.1.3	
-haxelib install tjson 1.4.0
+haxelib install lime 8.2.3 --skip-dependencies
+haxelib install openfl 9.4.2 --skip-dependencies
+haxelib install flixel 5.8.0 --skip-dependencies
+haxelib install flixel-addons 3.3.2 --skip-dependencies
+haxelib install flixel-tools 1.5.1 --skip-dependencies
+haxelib install hscript-iris 1.1.3	--skip-dependencies
+haxelib install tjson 1.4.0 --skip-dependencies
 haxelib install hxdiscord_rpc 1.2.4
 haxelib install hxvlc 2.0.1 --skip-dependencies
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
