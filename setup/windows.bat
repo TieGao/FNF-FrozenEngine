ad6b85ca1342bc17969eb13d6a37cb784b038468
TieGao <eeg2332019@outlook.com>
2026-03-25 13:23:47 +0800

    Frozen Engine 0.4.8 Prepare

--- setup/windows.bat
+++ setup/windows.bat
@@ -14,6 +14,7 @@ haxelib install hxvlc 2.2.5 --skip-dependencies
 haxelib install lime 8.3.1
 haxelib install openfl 9.5.1
 haxelib install moonchart 0.5.1
+haxelib git hxcpp https://github.com/TieGao/hxcpp.git
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
